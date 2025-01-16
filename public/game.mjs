de87eb7ac60c5b18b0e0e86a4998bc8e6ac09e2a
mateus-sartorio <mateussartorio39@gmail.com>
2025-01-15 23:43:47 -0300

    Wip: canvas tutorial - video 3 - text

--- public/game.mjs
+++ public/game.mjs
@@ -5,19 +5,27 @@ const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
 
-canvas.style.background = "#000";
+canvas.style.background = "yellow";
 
 class Circle {
-  constructor(x, y, radius, color, width = 1) {
+  constructor(x, y, radius, color, width, text) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.width = width;
+    this.text = text;
   }
 
-  draw() {
+  draw(context) {
     context.beginPath();
+
+    context.strokeStyle = this.color;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.font = `${this.radius/3}px Arial`
+    context.fillText(this.text, this.x, this.y);
+
     context.strokeStyle = this.color;
     context.lineWidth = this.width;
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -26,6 +34,5 @@ class Circle {
   }
 }
 
-const circle = new Circle(10, 10, 10, "white");
-debugger;
-circle.draw();
+const circle = new Circle(100, 100, 50, "black", 1, "cool text");
+circle.draw(context);
