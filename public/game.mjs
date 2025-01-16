359aae8f770caff9160648d6ef09ff1bc9f71341
mateus-sartorio <mateussartorio39@gmail.com>
2025-01-15 23:55:05 -0300

    Wip: canvas tutorial - video 4 - move objects around

--- public/game.mjs
+++ public/game.mjs
@@ -8,13 +8,15 @@ const context = canvas.getContext('2d');
 canvas.style.background = "yellow";
 
 class Circle {
-  constructor(x, y, radius, color, width, text) {
+  constructor(x, y, radius, color, width, text, speed) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.width = width;
     this.text = text;
+    this.dx = 1 * speed;
+    this.dy = 1 * speed;
   }
 
   draw(context) {
@@ -32,7 +34,31 @@ class Circle {
     context.stroke();
     context.closePath();
   }
+
+  update(context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.draw(context);
+
+    if(((this.x + this.radius) > canvas.width) || ((this.x - this.radius) < 0)) {
+      this.dx = -this.dx;
+    }
+
+    if(((this.y + this.radius) > canvas.height) || ((this.y - this.radius) < 0)) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+  }
 }
 
-const circle = new Circle(100, 100, 50, "black", 1, "cool text");
+const circle = new Circle(100, 100, 50, "black", 1, "cool text", 10);
 circle.draw(context);
+
+function update() {
+  requestAnimationFrame(update);
+  circle.update(context);
+}
+
+update();
