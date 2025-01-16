bd3bea7599c1da1214f2072f06876cda6d389c54
mateus-sartorio <mateussartorio39@gmail.com>
2025-01-15 23:24:12 -0300

    Wip: canvas tutorial - video 1

--- public/game.mjs
+++ public/game.mjs
@@ -1,8 +1,19 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
 
-// const socket = io();
+const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
 
-canvas.style.background = "#000";
+canvas.style.background = "#ff00ff";
+
+context.fillStyle = "#ff0000";
+context.fillRect(0, 0, 100, 100);
+context.fillRect(0, 0, 200, 200);
+
+context.beginPath();
+context.strokeStyle = "blue";
+context.lineWidth = 5;
+context.arc(100, 100, 50, 0, Math.PI * 2, false);
+context.stroke();
+context.closePath();
