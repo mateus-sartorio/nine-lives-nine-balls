09b4d6e6893c024e7fcb50ecd69227aa479ad2b2
mateus-sartorio <mateussartorio39@gmail.com>
2025-01-15 23:34:44 -0300

    Wip: canvas tutorial - video 2 - circle class

--- public/game.mjs
+++ public/game.mjs
@@ -5,15 +5,27 @@ const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
 
-canvas.style.background = "#ff00ff";
+canvas.style.background = "#000";
 
-context.fillStyle = "#ff0000";
-context.fillRect(0, 0, 100, 100);
-context.fillRect(0, 0, 200, 200);
+class Circle {
+  constructor(x, y, radius, color, width = 1) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.width = width;
+  }
 
-context.beginPath();
-context.strokeStyle = "blue";
-context.lineWidth = 5;
-context.arc(100, 100, 50, 0, Math.PI * 2, false);
-context.stroke();
-context.closePath();
+  draw() {
+    context.beginPath();
+    context.strokeStyle = this.color;
+    context.lineWidth = this.width;
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.stroke();
+    context.closePath();
+  }
+}
+
+const circle = new Circle(10, 10, 10, "white");
+debugger;
+circle.draw();
